d2599b6be7c229bf2bf0f96270baf426a7305367
Msafirjuma <Msafirijuma73gmail.com>
2023-08-08 10:19:35 +0300

    my project tasakhtaaApp

---
+++ android/app/src/main/java/com/dancingwhale/halodoctor/client/hallo_doctor_client/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dancingwhale.halodoctor.client.hallo_doctor_client;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+
+public class MainActivity extends FlutterFragmentActivity {
+}
